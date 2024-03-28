d7579de2439474c81c37559d219e60dc3fdb415a
waqas <wmunir232@gmail.com>
2024-03-28 14:28:55 +0500

    Created Loading Page

---
+++ app/loading.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+import { ClipLoader } from "react-spinners";
+const LoadingPage = ({ loading }) => {
+  return (
+    <ClipLoader
+      cssOverride={override}
+      size={150}
+      color="#3b82f6"
+      loading={loading}
+    />
+  );
+};
+
+export default LoadingPage;
