954d2878a758d309d6aca40eefe3fe16f90b1dd2
waqas <wmunir232@gmail.com>
2024-03-29 19:43:48 +0500

    Connected Mongodb

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import HomeProperty from "@/components/HomeProperty";
 import InfoBoxes from "@/components/InfoBoxes";
+import connectDB from "@/config/db";
 
 const Homepage = () => {
   return (
